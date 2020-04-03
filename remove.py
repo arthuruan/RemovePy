c9bc85f3d5b756fc2414d99e70bccee4e47f887c
Arthur <arthur123ruan@hotmail.com>
2020-04-02 23:36:24 -0300

    first commit

---
+++ remove.py
@@ -0,0 +1,23 @@
+from unidecode import unidecode
+
+def removeAdor(old, to_remove):
+    new_string = old
+    for x in to_remove:
+        new_string = new_string.replace(x, '')
+    return new_string
+
+folder = open('text.txt', 'r', encoding='utf8')
+
+text = folder.read()
+
+newText = text.lower()
+newText2 = unidecode(newText)
+
+newText3 = removeAdor(newText2, '/1234567890+\.><_:?!,;*()[]{}ªº¢¥¤ð-ßµÞ¦¶Ð+#@$%&^~´`«©■•"‘£\'')
+
+folderFomarted = open('textFormated.txt', 'w')
+
+folderFomarted.write(newText3)
+
+folder.close()
+folderFomarted.close()
